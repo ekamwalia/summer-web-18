d79b3a93897868c8646e4373b68b82ad83964aea
Arav Saraff <aravsaraff@gmail.com>
2018-05-09 01:13:13 +0530

    Task: 1

--- Quotes/app.py
+++ Quotes/app.py
@@ -7,17 +7,17 @@ quotes = ['quote1', 'quote2']
 
 @app.route('/addquote', methods=['POST'])
 def add_quote():
-	new_quote = request.json
+	new_quote = request.json['quote']
 	quotes.append(new_quote)
 	return jsonify(quotes)
 
 @app.route('/getquotes', methods=['GET'])
 def get_quotes():
-	return jsonify(quotes)
+	return jsonify({'quote': quotes})
 
 @app.route('/randomquote', methods=['GET'])
 def random_quote():
-    return jsonify(random.choice(quotes))
+    return jsonify({'quote': random.choice(quotes)})
 
 if __name__=='__main__':
 	app.run(debug = True)
