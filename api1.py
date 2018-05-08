b4a5ec84a243c8e980af4428e48fe3c733223fad
Anutrix <numaanzaheerahmed@yahoo.com>
2018-05-09 00:48:54 +0530

    all and random1 quote functions added

---
+++ api1.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+import random
+app = Flask(__name__)
+
+quotes = ["uno", "dos", "tres", "cuantro", "cinco"]
+
+@app.route("/")
+def intro():
+	return '1. Add a new quote(/add) 2. Get a random​ quote(/random1) 3. Get all available quotes.(/all)'
+
+#@app.route("/add", methods=['POST'])
+#def add():
+#	q = request.json['quotes']
+#	quotes.append(q)
+#	return jsonify({'quote added':q})
+	
+@app.route("/random1", methods=['GET'])
+def random_quote():
+	return jsonify(quotes[random.randrange(len(quotes))])
+	
+@app.route("/all", methods=['GET'])
+def print_all():
+	return jsonify(quotes)
