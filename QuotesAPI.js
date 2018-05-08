cb4195c642afeb7c25ea79685d680c7084eec06d
Soumik Majumder <soumik712@gmail.com>
2018-05-09 00:49:56 +0530

    Changes made to QuotesAPI after review

--- QuotesAPI.js
+++ QuotesAPI.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.listen(3000);
+app.listen(3000, (err)=>{
+	if(err)
+		console.log(err);
+	else
+		console.log('Listening to port 3000');
+});
 app.use(bodyParser.json());
 
 var cont = ['Lol noob','GG','ez'];
@@ -15,10 +20,12 @@ app.get('/rand',(req,res)=>{
 
 app.delete('/delete/:id',(req,res)=>{
 	var delId = req.params.id -1 ;
-	cont.splice(delID,1);
+	cont.splice(delId,1);
+	res.json({"message":"quote deleted"});
 });
 
 app.post('/add',(req,res)=>{
 	var val = req.body;
 	cont.push(val.quote);
+	res.json({"message":"quote added"});
 });
