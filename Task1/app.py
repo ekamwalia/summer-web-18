c22cdacafea0cf96c056f3b521a6294dadfabb2f
anshitabaid <anshita_b@yahoo.co.in>
2018-05-09 23:18:35 +0530

    Task 1

---
+++ Task1/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, json
+from random import randint
+app=Flask(__name__)
+
+quotes=["abc", "cde", "fgh"]
+
+@app.route ("/")
+def hello():
+	return "Add a quote: /add \n Random Quote: /random \n All available quotes: /all"
+
+@app.route ("/add", methods=['GET', 'POST'])
+def add():
+	quotes.append ("appended quote")
+	return "Quote appended"
+
+@app.route ("/random")
+def random():
+	return json.dumps (quotes[randint (0, len(quotes)-1)]);
+
+@app.route ("/all")
+def all():
+	return json.dumps(quotes)
+
+
+if __name__=="__main__":
+	app.run()
