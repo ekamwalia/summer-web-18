2d4506dad258d67d00bd119b10a0309fbc48c01a
srinchow <32598542+srinchow@users.noreply.github.com>
2018-05-17 00:10:36 +0530

    Task1
    Made a folder earlier was a single file

---
+++ Task1/task1finalapi.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+import json
+from flask import request
+import random
+app = Flask(__name__)
+
+qoute = ["q1","q2","q3","q4"]
+
+@app.route('/')
+def intro():
+    return 'Welcome to this page please enter a preffered choice 1)generate a random qoute,2)add a qoute,3)show all qoutes'
+
+@app.route('/hello')
+def hello():
+	return 'HELLO WORLD!!'
+
+@app.route('/allqoutes',methods=['GET'])
+def all_qoutes():
+	return jsonify(qoute)
+
+
+@app.route('/randqoute',methods=['GET'])
+def rand_qoutes():
+	return jsonify({'quote': random.choice(qoute)})
+
+@app.route('/addnew',methods=['POST'])
+def new():
+	data=request.json['quote']
+	qoute.append(data)
+	return jsonify({'quote added':data})
+
+
+
+
