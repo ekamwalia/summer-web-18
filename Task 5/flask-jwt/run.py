2e408ff66b1ea194c7e8d1775228564e82a37fa4
JyotinderSingh <jyotindrsingh@gmail.com>
2018-06-26 23:41:10 +0530

    Task 5 API complete

---
+++ Task 5/flask-jwt/run.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '#SuperSecure_~.~_'
+
+app.config['JWT_SECRET_KEY'] = '#This_is_pretty_secure~_~'
+
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
+
+@app.before_first_request
+def create_tables():
+	db.create_all()
+
+
+import views, models, resources
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogoutAccess, '/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.AllUsers, '/users')
+api.add_resource(resources.SecretResource, '/secret')
+
---
+++ Task 5/flask-jwt/venv/lib/python3.6/__future__.py
@@ -0,0 +1,1 @@
+/home/jyotinder/anaconda3/lib/python3.6/__future__.py
