39af39eed78821f37465b10cc7346e1bf81f496b
anshitabaid <anshita_b@yahoo.co.in>
2018-07-08 20:42:53 +0530

    error

--- Task6/login.py
+++ Task6/login.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, jsonify, request, redirect, url_for, escape
+from flask import Flask, session, jsonify, request, redirect, url_for, escape,render_template
 from flask_session import Session
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -6,7 +6,7 @@ from passlib.hash import sha256_crypt
 
 
 project_dir=os.path.dirname(os.path.abspath(__file__))
-database_file="sqlite:///{}".format(os.path.join(project_dir, "cruddb.db"))
+database_file="sqlite:///{}".format(os.path.join(project_dir, "appdb.db"))
 app=Flask (__name__)
 sess=Session()
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
@@ -22,17 +22,36 @@ class Users (db.Model):
 
 @app.route ('/register', methods=['GET', 'POST'])
 def register ():
-    data=request.json
+    if request.method == 'POST':
+        data=request.form
+        #username=data['username']
+        #password=data['password']
+    
     user=Users (data ['username'], data['password'])
     db.session.add (user)
     db.session.commit ()
-    return jsonify (data)
+    return render_template ('signup.html')
+    
+@app.route ("/signin", methods=['GET', 'POST'])
+def signin():
+    #if request.method == 'POST':
+    if request.method == 'POST':
+        data=request.form
+    user=Users.query.filter_by (username=data['username']).first ()
+    if sha256_crypt.verify (data['password'], user.password):
+        #login successful 
+        session['username']=data['username'] #set session variable
+        return redirect(url_for ('index'))
+    return render_template('signin.html')
 
+
+'''
 @app.route ("/login", methods=['GET', 'POST'])
 def login():
     if 'username' in session:
         return (session['username'] + " is logged in")
-    data=request.json 
+    #render_template ('signin.html')
+    data=request.form
     user=Users.query.filter_by (username=data['username']).first ()
     if sha256_crypt.verify (data['password'], user.password):
         #login successful 
@@ -40,12 +59,18 @@ def login():
         return redirect(url_for ('index'))
     else:
         return jsonify({'Login': 'False'})
+        
 
 @app.route ("/index", methods=['GET', 'POST'])
 def index():
     if 'username' in session:
         return 'You are logged in as %s' % escape (session['username'])
     return 'You are not logged in.'
+    '''
+
+@app.route("/index", methods=['GET', 'POST'])
+def index ():
+    return 'hi there'
 
 @app.route ("/logout",methods=['GET', 'POST'])
 def logout():
