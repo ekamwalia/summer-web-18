0ecc6a50bb40c666730445383641a025b9f19400
anshitabaid <anshita_b@yahoo.co.in>
2018-07-10 01:00:15 +0530

    Added signin, signout and repository search

--- Task6/login.py
+++ Task6/login.py
@@ -3,6 +3,7 @@ from flask_session import Session
 import os
 from flask_sqlalchemy import SQLAlchemy
 from passlib.hash import sha256_crypt
+import requests
 
 
 project_dir=os.path.dirname(os.path.abspath(__file__))
@@ -22,28 +23,49 @@ class Users (db.Model):
 
 @app.route ('/register', methods=['GET', 'POST'])
 def register ():
+    status=''
     if request.method == 'POST':
         data=request.form
         #username=data['username']
         #password=data['password']
-    
-    user=Users (data ['username'], data['password'])
-    db.session.add (user)
-    db.session.commit ()
-    return render_template ('signup.html')
+        user=Users (data ['username'], data['password'])
+        try:
+            db.session.add(user)
+            db.session.commit()
+            status = 'Success'
+        except:
+            status = 'This user is already registered'
+        db.session.close()
+    return render_template ('signup.html', status=status)
     
 @app.route ("/signin", methods=['GET', 'POST'])
 def signin():
-    #if request.method == 'POST':
+    #if request.method == 'POST'
+    if 'username' in session:
+        return 'User is already signed in'
     if request.method == 'POST':
         data=request.form
-    user=Users.query.filter_by (username=data['username']).first ()
-    if sha256_crypt.verify (data['password'], user.password):
-        #login successful 
-        session['username']=data['username'] #set session variable
-        return redirect(url_for ('index'))
+        user=Users.query.filter_by (username=data['username']).first ()
+        if sha256_crypt.verify (data['password'], user.password):
+            #login successful 
+            session['username']=data['username'] #set session variable
+            return redirect(url_for ('homepage'))
     return render_template('signin.html')
 
+@app.route ("/homepage", methods=['GET', 'POST'])
+def homepage ():
+    if 'username' not in session:
+        return 'Access denied'
+    if request.method=='POST':
+        user=request.form['search']
+        data=requests.get ('https://api.github.com/users/'+user +'/repos')
+        details=data.json() #list
+        repolist=[]
+        for repo in details:
+            repolist.append (repo['name'])
+        print (repolist)
+        return render_template('homepage.html', username=session['username'], details=repolist)
+    return render_template('homepage.html', username=session['username'])
 
 '''
 @app.route ("/login", methods=['GET', 'POST'])
@@ -72,11 +94,11 @@ def index():
 def index ():
     return 'hi there'
 
-@app.route ("/logout",methods=['GET', 'POST'])
-def logout():
+@app.route ("/signout",methods=['GET', 'POST'])
+def signout():
     if 'username' in session:
         session.pop ('username', None)
-        return 'You are logged out'  
+        return 'You have been signed out'  
     else:
         return ('You were never logged in')   
 
